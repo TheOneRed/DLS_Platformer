085c01aa8dfed091183098d1ade2d019da86d997
Jonathan Lee <jonlee50@hotmail.com>
2017-09-25 22:44:36 -0400

    Next Level and Sounds
    Goal goes to next level
    Reaching goal generates sound

---
+++ DLS_Platformer/Assets/_Scripts/NextLevel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevel : MonoBehaviour {
+
+	public AudioSource winSound;
+
+	// Use this for initialization
+	void Start () {
+		winSound = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D Man)
+	{
+		if (Man.gameObject.tag == "Player") 
+		{
+			Invoke ("GoToNextLevel", 0.5f);
+			winSound.Play ();
+		}
+	}
+
+	public void GoToNextLevel()
+	{
+		Application.LoadLevel ("HurryUpFranco");
+	}
+}
