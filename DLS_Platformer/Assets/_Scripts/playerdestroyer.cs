b70444d45312539283ca7f5f247a48e9076341c8
Franco Chong <fnbeddie@hotmail.com>
2017-10-04 15:00:39 -0400

    minor changes

---
+++ DLS_Platformer/Assets/_Scripts/playerdestroyer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerdestroyer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	//reloads the scene upon collision with the player.
+	void OnTriggerEnter2D(Collider2D otherCollider){
+
+		if (otherCollider.gameObject.CompareTag("Player")){
+			Application.LoadLevel(Application.loadedLevel);
+		}
+
+	}
+}
