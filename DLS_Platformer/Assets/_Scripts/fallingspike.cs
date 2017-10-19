5062bfbd452ff280d925f65fc7c073f51c0d654f
Franco Chong <fnbeddie@hotmail.com>
2017-10-18 20:58:11 -0400

    level 3 with flying enemies placeholders added

---
+++ DLS_Platformer/Assets/_Scripts/fallingspike.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingspike : MonoBehaviour {
+
+	public GameObject spikes;
+
+	public float moveSpeed;
+	public Transform current;
+
+	public Transform[] location;
+
+	public int selection;
+
+	// Use this for initialization
+	void Start () {
+		current = location [selection];
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+	}
+
+	void OnTriggerEnter2D(Collider2D otherCollider){
+
+		if (otherCollider.gameObject.CompareTag("Player")){
+			spikes.transform.position = Vector3.MoveTowards(spikes.transform.position, current.position, Time.deltaTime * moveSpeed);
+			if (spikes.transform.position == current.position) 
+			{
+				selection++;
+			}
+		}
+
+	}
+
+}
---
+++ DLS_Platformer/Assets/_Scripts/movingplatform.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingplatform : MonoBehaviour {
+
+	public GameObject platform;
+
+	public float moveSpeed;
+	public Transform current;
+
+	public Transform[] locations;
+
+	public int selection;
+
+
+	// Use this for initialization
+	void Start () {
+		current = locations [selection];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		platform.transform.position = Vector3.MoveTowards(platform.transform.position, current.position, Time.deltaTime * moveSpeed);
+		if (platform.transform.position == current.position) 
+		{
+			selection++;
+			if (selection == locations.Length)
+			{
+				selection = 0;
+			}
+			current = locations [selection];
+		}
+	}
+}
