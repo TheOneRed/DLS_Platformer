5a42dc0349559eec4b360a387e678531c85603fd
Jonathan Lee <jonlee50@hotmail.com>
2017-10-27 19:54:20 -0400

    Rotating Platforms Sorta
    Its sorta rotating platforms

---
+++ DLS_Platformer/Assets/_Scripts/CircularPlatform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularPlatform : MonoBehaviour {
+
+
+	public GameObject centerOrbit;
+	public float speed = 10;
+	float lockPlatform = 0;
+
+	//Vector3 startingPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Awake(){
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.RotateAround (centerOrbit.transform.position, Vector3.forward, speed);
+		transform.rotation = Quaternion.Euler(lockPlatform, lockPlatform, lockPlatform);
+	}
+}
