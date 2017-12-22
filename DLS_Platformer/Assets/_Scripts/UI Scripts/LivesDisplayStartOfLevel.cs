3f0a0ad3bc2c967d839abc1b10199f12f9db8b2a
Jonathan Lee <jonlee50@hotmail.com>
2017-12-22 01:38:08 -0500

    Lives Display at start of levels
    Lives display at start of level for 5 sec before level begins
    actual lives number not yet synced

---
+++ DLS_Platformer/Assets/_Scripts/UI Scripts/LivesDisplayStartOfLevel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LivesDisplayStartOfLevel : MonoBehaviour {
+
+	//PUBLIC VARIABLES
+	public GameObject LivesUI;
+	public GameObject LivesLvStartUI;
+	public bool isScene = true;
+
+	// Use this for initialization
+	void Start () {
+		//if (SceneManager.GetActiveScene ().name == "BossBattle1") 
+		if (isScene == true)
+		{
+			LivesUI.SetActive (false);
+			LivesLvStartUI.SetActive (true);
+			Time.timeScale = 0f;
+			Invoke ("DeactivateLivesLvStartUI", 5.0f);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (LivesLvStartUI.activeInHierarchy == true) 
+			if(isScene == true)
+		{
+			Debug.Log ("TimeScale should be 0");
+			Time.timeScale = 0f;
+		} 
+		//if(LivesLvStartUI.activeInHierarchy == false)
+			if(isScene == false)
+		{
+			Time.timeScale = 0f;
+		}
+	}
+
+	void DeactivateLivesLvStartUI()
+	{
+		LivesLvStartUI.SetActive (false);
+		LivesUI.SetActive (true);
+		Time.timeScale = 0f;
+		isScene = false;
+		Debug.Log("TimeScale should be 1");
+	}
+}
---
+++ DLS_Platformer/Assets/_Scripts/UI Scripts/PauseMenu.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+	
+	public string mainMenu;
+
+	public bool isPaused;
+
+	public GameObject pauseMenu;
+
+	public GameObject LivesUI;
+	public GameObject LivesLvStartUI;
+	public GameObject player;
+	public GameObject enemy;
+
+	public bool isScene = true;
+
+	void Start () {
+		//if (SceneManager.GetActiveScene ().name == "BossBattle1" || SceneManager.GetActiveScene ().name == "Level 3 flying") 
+		if (isScene == true)
+		{
+			LivesUI.SetActive (false);
+			LivesLvStartUI.SetActive (true);
+			player.SetActive (false);
+			enemy.SetActive (false);
+			//Time.timeScale = 0f;
+			Invoke ("DeactivateLivesLvStartUI", 5.0f);
+			//StartCoroutine(Example());
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		if (isPaused) {
+			pauseMenu.SetActive (true);
+			Time.timeScale = 0f;
+		} else {
+			pauseMenu.SetActive (false);
+			Time.timeScale = 1f;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			isPaused = !isPaused;
+		}
+
+		if (LivesLvStartUI.activeInHierarchy == true) 
+		//if(isScene == true)
+		{
+			Debug.Log ("TimeScale should be 0");
+			//Time.timeScale = 0f;
+
+		} 
+		else if(LivesLvStartUI.activeInHierarchy == false)
+		//if(isScene == false)
+		{
+			//Time.timeScale = 1f;
+		}
+	}
+		
+	public void Resume(){
+		isPaused = false;
+	}
+
+	public void Restart(){
+		Debug.Log ("work pls");
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+	public void Quit(){
+		Application.LoadLevel (mainMenu);
+	}
+
+	void DeactivateLivesLvStartUI()
+	{
+		LivesLvStartUI.SetActive (false);
+		LivesUI.SetActive (true);
+		player.SetActive (true);
+		enemy.SetActive (true);
+		//Time.timeScale = 1f;
+		isScene = false;
+		Debug.Log("TimeScale should be 1");
+	}
+
+	IEnumerator Example()
+	{
+		yield return new WaitForSeconds (5.0f);
+		LivesLvStartUI.SetActive (false);
+		LivesUI.SetActive (true);
+		//Time.timeScale = 1f;
+		//isScene = false;
+		Debug.Log("TimeScale should be 1");
+	}
+}
